d1e90515df2babad5679613da57e90b0ce69155c
Joao Guis <joao.guis2001@gmail.com>
2022-08-26 20:26:55 -0300

    init springboot example

---
+++ src/main/java/br/pucbr/exemplo/usuario/entity/Usuario.java
@@ -0,0 +1,58 @@
+package br.pucbr.exemplo.usuario.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USUARIO")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "NOME", nullable = false)
+    private String nome;
+
+    @Column(name = "LOGIN", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "SENHA", nullable = false)
+    private String senha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/main/java/br/pucbr/exemplo/usuario/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package br.pucbr.exemplo.usuario.service;
+
+import br.pucbr.exemplo.usuario.entity.Usuario;
+import br.pucbr.exemplo.usuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> listar() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario buscarPorId(Integer id) {
+        return usuarioRepository.findById(id).get();
+    }
+
+    public void excluir(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+
+}
